8c7593fec31a01f6483406cc3e0c6f597d02f9e7
frousseu <francois.rousseu@usherbrooke.ca>
2017-03-10 14:20:22 -0500

    used ajusted ggbiplot2 and produced pdf output for lm model with an interaction

--- RBradley/mahalanobsis.R
+++ RBradley/mahalanobsis.R
@@ -23,11 +23,11 @@ d<-as.data.frame(t(d),stringsAsFactors=FALSE)
 names(d)<-unname(d[1,])
 d<-d[-1,]
 names(d)[1]<-"plot"
-d$sample<-substr(d$plot,nchar(d$plot),nchar(d$plot))
+d$sample<-paste0("s",substr(d$plot,nchar(d$plot),nchar(d$plot)))
 d$plot<-gsub("i","I",substr(d$plot,1,2))
 d$season<-sapply(strsplit(rownames(d),"_"),function(i){i[1]})
 d$year<-as.integer(substr(sapply(strsplit(rownames(d),"_"),function(i){i[2]}),1,4))
-d$pair<-ifelse(d$plot%in%c("SI","BI"),1,2)
+d$pair<-ifelse(d$plot%in%c("SI","BI"),"BI_SI","SR_CO")
 d$time<-match(paste0(d$season,d$year),c("spring2009","fall2009","spring2010","fall2010","spring2011"))
 keep<-c("plot","pair","sample","season","year","time")
 d<-d[,c(keep,setdiff(names(d),keep))]
@@ -48,30 +48,75 @@ x<-ddply(d,.(pair,season,year,time),function(i){
   y<-as.matrix(i[w2,-seq_along(keep)])[,1:n][,1:nn]
   center<-colMeans(y)
   SigmaInv<-solve(var(d[,-seq_along(keep)][,1:n][,1:nn]))
-  sqrt(apply(x,1,function(i){(i-center) %*% SigmaInv %*% (i-center)}))
+  res<-sqrt(apply(x,1,function(i){(i-center) %*% SigmaInv %*% (i-center)}))
+  names(res)<-paste0("s",seq_along(res))
+  res
   #sqrt(mahalanobis(x,center=unname(center),cov=SigmaInv,tol=1e-80,inverted=TRUE))
 })
 x<-x[order(x$pair,x$year,ifelse(x$season=="spring",1,2)),]
 #x$time<-rep(1:5,2)
-x<-x[,c("pair","season","year","time","6","7","8","9","10")]
+x<-x[,c("pair","season","year","time","s1","s2","s3","s4","s5")]
 
-
+png("C:/Users/rouf1703/Documents/UdeS/Consultation/RBradley/Prog/maha.png",units="in",res=500,width=8,height=8)
 #windows()
-plot(1:5,1:5,ylim=c(0,max(as.numeric(unlist(x[,-(1:4)])),na.rm=TRUE)),type="n",xaxt="n",yaxt="n",xlab="Time",ylab="Mahalanobis Distance")
+plot(1:5,1:5,ylim=c(2,max(as.numeric(unlist(x[,-(1:4)])),na.rm=TRUE)),type="n",xaxt="n",yaxt="n",xlab="Time",ylab="Mahalanobis Distance")
 axis(1,at=1:5,label=paste(x$season[1:5],x$year[1:5],sep="_"))
 axis(2,las=2)
-for(i in 1:5){points(rep(x$time[i],5),unlist(x[i,5:9]),col=alpha("blue",0.5),pch=1,lwd=2,cex=1.5)}
-for(i in 6:10){points(rep(x$time[i],5),unlist(x[i,5:9]),col=alpha("red",0.5),pch=1,lwd=2,cex=1.5)}
+for(i in 1:5){points(rep(x$time[i],5),unlist(x[i,5:9]),col="blue",pch=1,lwd=2,cex=1.5)}
+for(i in 6:10){points(rep(x$time[i],5),unlist(x[i,5:9]),col="red",pch=1,lwd=2,cex=1.5)}
 x2<-ddply(x,.(pair,time),function(i){
   cbind(i[rep(1,5),1:4],dm=unlist(i[1,5:9]))
 })
-m1<-lm(dm~time,data=x2[x2$pair==1,])
-m2<-lm(dm~time,data=x2[x2$pair==2,])
-abline(m1,col=alpha("blue",0.5),lwd=3)
-abline(m2,col=alpha("red",0.5),lwd=3)
-legend("topright",pch=1,lwd=3,pt.cex=1.5,col=alpha(c("red","blue",0.5)),legend=c("BI vs. SI","SR vs. CO"))
-#visreg(m1)
-#visreg(m2)
+m<-lm(dm~time*pair,data=x2)
+m1<-lm(dm~time,data=x2[x2$pair=="BI_SI",])
+m2<-lm(dm~time,data=x2[x2$pair=="SR_CO",])
+newdata<-expand.grid(time=1:5,pair=unique(x2$pair))
+p<-predict(m,newdata)
+lines(newdata$time[1:5],p[1:5],col="blue")
+lines(newdata$time[6:10],p[6:10],col="red")
+abline(m1,col=alpha("blue",0.5),lwd=2)
+abline(m2,col=alpha("red",0.5),lwd=2)
+legend("topright",pch=1,lwd=2,pt.lwd=2,pt.cex=1.5,col=c("red","blue"),legend=c("BI => SI","SR => CO"))
+dev.off()
+
+write.csv(x2,"C:/Users/rouf1703/Documents/UdeS/Consultation/RBradley/Prog/maha.csv",row.names=FALSE)
+### output lm result to pdf
+stargazer(m,ci=TRUE,intercept.bottom=FALSE,single.row=TRUE)
+
+
+
+#visreg(m,"time",by="pair",overlay=TRUE,cex=20,points=list(cex=1),fill.par=list(col=NA,border="black"))
+
+
+
+
+
+### pca
+temp<-d[d$plot%in%c("BI","SI"),]
+pca<-prcomp(temp[,7:ncol(d)],scale=TRUE)
+#mod<-rda(temp[,7:ncol(d)],scale=TRUE)
+par(mar=c(2,2,2,2))
+biplot(pca,col=c("white","grey80"),expand=1.5,xlim=range(pca$rot[,1]),ylim=range(pca$rot[,2]))
+box("plot",col="black")
+#biplot(pca$rotation,pca$x,col=c("grey80","grey80"),expand=0.5)
+text(pca$x[,1],pca$x[,2],temp$plot,col=alpha(temp$time+1,0.5),font=2,cex=1.25)
+#text(mod$CA$v[,1],mod$CA$v[,2],temp$plot,col=temp$time+1,font=2)
+#points(pca$x[,1],pca$x[,2],col=temp$time,cex=4,pch=ifelse(temp$plot%in%"SI",1,2))
+legend("topleft",legend=unique(paste(temp$time,temp$season,temp$year,sep=" ")),fill=alpha(unique(temp$time)+1,0.5),cex=1.5,border=NA,bty="n")
+r<-range(pca$x[,1])
+
+png("C:/Users/rouf1703/Documents/UdeS/Consultation/RBradley/Prog/BI_SI_biplot.png",units="in",res=500,width=8,height=8)
+### ggplot biplot pca
+g<-ggbiplot2(pca,choices=1:2,obs.scale=1,var.scale=1,groups=paste(temp$time),ellipse=TRUE,circle=FALSE,labels=NULL,labels.size=5)
+g<-g+scale_color_discrete(name='Date',label=unique(paste(temp$season,temp$year)))
+g<-g+scale_shape_manual(values=c(16,10),name="Treatment")
+g<-g+theme_light()
+g<-g+theme(legend.direction='vertical',legend.position='right',panel.grid=element_blank(),panel.border=element_rect(colour="black"))
+g<-g+ylim(-4.5,3.5)
+#g<-g+xlim(r[1],r[2])
+print(g)
+#ggscreeplot(pca)
+dev.off()
 
 ### pca
 temp<-d[d$plot%in%c("SR","CO"),]
@@ -85,6 +130,20 @@ text(pca$x[,1],pca$x[,2],temp$plot,col=alpha(temp$time+1,0.5),font=2,cex=1.25)
 #text(mod$CA$v[,1],mod$CA$v[,2],temp$plot,col=temp$time+1,font=2)
 #points(pca$x[,1],pca$x[,2],col=temp$time,cex=4,pch=ifelse(temp$plot%in%"SI",1,2))
 legend("topleft",legend=unique(paste(temp$time,temp$season,temp$year,sep=" ")),fill=alpha(unique(temp$time)+1,0.5),cex=1.5,border=NA,bty="n")
+r<-range(pca$x[,1])
+
+png("C:/Users/rouf1703/Documents/UdeS/Consultation/RBradley/Prog/CO_SR_biplot.png",units="in",res=500,width=8,height=8)
+### ggplot biplot pca
+g<-ggbiplot2(pca,choices=1:2,obs.scale=1,var.scale=1,groups=paste(temp$time),ellipse=TRUE,circle=FALSE,labels=NULL,labels.size=5)
+g<-g+scale_color_discrete(name='Date',label=unique(paste(temp$season,temp$year)))
+g<-g+scale_shape_manual(values=c(16,10),name="Treatment")
+g<-g+theme_light()
+g<-g+theme(legend.direction='vertical',legend.position='right',panel.grid=element_blank(),panel.border=element_rect(colour="black"))
+g<-g+xlim(-5.1,3.25)
+print(g)
+#ggscreeplot(pca)
+dev.off()
+
 
 
 ### betadisper
@@ -113,14 +172,6 @@ plot(mod, axes = c(2,1), seg.col = "forestgreen", seg.lty = "dashed")
 
 ## Draw a boxplot of the distances to centroid for each group
 boxplot(mod)
-
-
-### ggplot biplot pca
-g<-ggbiplot(pca,choices=1:2,obs.scale=1,var.scale=1,groups=paste(temp$time),ellipse=TRUE,circle=FALSE,labels=temp$plot,labels.size=5)
-g<-g+scale_color_discrete(name='YY')
-g<-g+theme(legend.direction='horizontal',legend.position='top')
-print(g)
-#ggscreeplot(pca)
 
 
 
