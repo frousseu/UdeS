5dedd6e36d6fa1412a04144b4c408550a687e145
frousseu <francoisrousseu@hotmail.com>
2017-05-13 11:39:59 -0400

    predictions on landscape raster

--- M-LLecuyer/binom_geostat.R
+++ M-LLecuyer/binom_geostat.R
@@ -15,6 +15,11 @@ library(raster)
 library(visreg)
 library(PrevMap)
 library(bbmle)
+library(raster)
+library(sp)
+library(rgdal)
+library(rgeos)
+library(velox)
 
 
 
@@ -41,32 +46,82 @@ d$x<-d$X
 d$y<-d$Y
 
 
-#####################################
-### data and grid for predictions
-#####################################
+#########################################
+### import raster and build pred grid
+#########################################
 
-sca<-c("_p","_m","_g","tg")
-base<-c("Attack","Cat_Typ","Dens_Liv")
+r1<-"C:/Users/User/Documents/Lou/Mature_forest_2015_include_bajos_secondary.tif"
+r2<-"C:/Users/User/Documents/Lou/Mature_forest_2015_not_include_bajos_secondary.tif"
+r3<-"C:/Users/User/Documents/Lou/Landcover_2015_extended.tif"
+code<-read.table("C:/Users/User/Documents/Lou/Legend.txt",header=TRUE,sep=",",stringsAsFactors=FALSE)
+r <- stack(r3)
 
-### prediction grid
-ds$Cat_Typ<-as.factor(ds$Cat_Typ)
-e <- extent(coordinates(ds))
-r <- raster(e, ncol=50, nrow=50)
-vars<-c(names(d)[grep(sca[4],names(d))],"Dens_Liv")
-x<-rasterize(ds,r,vars,fun=mean)
-plot(x)
-g<-as(x,"SpatialPixelsDataFrame")
-proj4string(g)<-CRS(proj4string(ds))
+### build pred grid
+ras <- raster(xmn=bbox(r)[1,1],xmx=bbox(r)[1,2],ymn=bbox(r)[2,1],ymx=bbox(r)[2,2],ncols=100,nrows=100)
+ras[] <- runif(ncell(ras))
+plot(ras)
+g<-as(ras,"SpatialPixelsDataFrame")
+proj4string(g)<-proj4string(r)
+w<-2000
+p<-gBuffer(SpatialPoints(coordinates(g)),width=w,byid=TRUE)
+proj4string(p)<-proj4string(r)
+text(coordinates(p)[,1],coordinates(p)[,2],1:length(p))
 
-### newdata
-newdata<-g@data[,"P_indFor_tg",drop=FALSE]
+### extract values from grid
+v<-velox(r)
+e<-v$extract(p,fun=function(x){paste(x,collapse="_")})
+
+### compute proportions
+ans<-lapply(e,function(i){
+	x<-table(unlist(strsplit(i,"_")))
+	s<-setdiff(code$Code,names(x))
+	if(length(s)==0){
+		x<-x[order(names(x))]
+	}else{
+		temp<-rep(0,length(s))
+		names(temp)<-s
+		x<-c(x,temp)
+		x<-x[order(names(x))]
+	}
+	100*x/sum(x)
+})
+land<-as.data.frame(do.call("rbind",ans))
+names(land)<-gsub(" ","_",code$Category[match(names(land),code$Code)])
+g@data<-land
+
+
+### get proportions at obs
+p<-gBuffer(SpatialPoints(coordinates(ds)),width=w,byid=TRUE)
+proj4string(p)<-proj4string(r)
+eobs<-v$extract(p,fun=function(x){paste(x,collapse="_")})
+ans<-lapply(eobs,function(i){
+	x<-table(unlist(strsplit(i,"_")))
+	s<-setdiff(code$Code,names(x))
+	if(length(s)==0){
+		x<-x[order(names(x))]
+	}else{
+		temp<-rep(0,length(s))
+		names(temp)<-s
+		x<-c(x,temp)
+		x<-x[order(names(x))]
+	}
+	100*x/sum(x)
+})
+obs<-as.data.frame(do.call("rbind",ans))
+names(obs)<-gsub(" ","_",code$Category[match(names(obs),code$Code)])
+d<-cbind(d,obs,stringsAsFactors=FALSE)
+
+
+### newdata and model
+model<-Attack~Secondary
+newdata<-g@data[,"Secondary",drop=FALSE]
 
 
 #####################################
 ### simple glm without RSA
 #####################################
 
-glm1<-glm(Attack~P_indFor_tg,data=d,family=binomial)
+glm1<-glm(model,data=d,family=binomial)
 pred_glm<-predict(glm1,newdata,type="response")
 
 
@@ -89,7 +144,7 @@ dat<-as.data.frame(d)[,-1]
 names(dat)[1:2]<-c("x","y")
 
 ### model
-gsif<-fit.regModel(Attack ~ P_indFor_tg,rmatrix=dat,predictionDomain=g,method="GLM",fit.family=binomial(link="logit"),stepwise=FALSE,vgmFun="Exp")
+gsif<-fit.regModel(model,rmatrix=dat,predictionDomain=g,method="GLM",fit.family=binomial(link="logit"),stepwise=FALSE,vgmFun="Exp")
 
 ### predictions
 pred_gsif<-predict(gsif,g)
@@ -112,11 +167,11 @@ control.mcmc<-control.mcmc.MCML(n.sim=10000,burnin=1000,thin=8,h=NULL,c1.h = 0.0
 par0<-c(coef(glm1),c(0.99,5700,0.15)) # inputs from the variogram
 
 ### first evaluation
-mcml1<-binomial.logistic.MCML(formula=Attack~P_indFor_tg,units.m=~nbevent,par0=par0,coords=~X+Y,data=d,control.mcmc=control.mcmc,kappa=0.5,start.cov.pars=c(par0[4],par0[5]/par0[3]))
+mcml1<-binomial.logistic.MCML(model,units.m=~nbevent,par0=par0,coords=~X+Y,data=d,control.mcmc=control.mcmc,kappa=0.5,start.cov.pars=c(par0[4],par0[5]/par0[3]))
 
 ### need a second estimation with starting values from the first evaluation (don't understand why)
 par1<-coef(mcml1)
-mcml1<-binomial.logistic.MCML(formula=Attack~P_indFor_tg,units.m=~nbevent,par0=par1,coords=~X+Y,data=d,control.mcmc=control.mcmc,kappa=0.5,start.cov.pars=c(par1[4],par1[5]/par1[3]))
+mcml1<-binomial.logistic.MCML(model,units.m=~nbevent,par0=par1,coords=~X+Y,data=d,control.mcmc=control.mcmc,kappa=0.5,start.cov.pars=c(par1[4],par1[5]/par1[3]))
 
 ### prediction
 pred_mcml <- spatial.pred.binomial.MCML(mcml1,coordinates(g),predictors=newdata,control.mcmc = control.mcmc, type = "marginal",scale.predictions = "prevalence",standard.errors = TRUE, thresholds = 0.2,scale.thresholds = "prevalence")
@@ -134,16 +189,18 @@ pred_mcml <- spatial.pred.binomial.MCML(mcml1,coordinates(g),predictors=newdata,
 # MLE based on the Laplace approximation
 
 ### source code is their supplementary material
-source("C:/Users/rouf1703/Documents/UdeS/GitHub/UdeS/M-LLecuyer/functionssglmm.r")
+source("C:/Users/User/Documents/GitHub/UdeS/M-LLecuyer/functionssglmm.r")
 
 ### start values
-init <- start.values.glgm(Attack ~ P_indFor_tg, family="binomial", data=d[,c("x","y","Attack","P_indFor_tg")], coords=d[,c("x","y")],nugget=TRUE, ntrial=1)
+init <- start.values.glgm(model, family="binomial", data=d[,c("x","y",colnames(model.frame(model,d)))], coords=d[,c("x","y")],nugget=TRUE, ntrial=1)
 
 ### Fitting Binomial SGLMM
-glgm1 <- glgm(Attack ~ P_indFor_tg, cov.model = "matern", kappa = log(0.5), inits = init, data=d, coords = d[,c("x","y")],nugget=TRUE, family="binomial", ntrial = 1,method.optim = "BFGS", method.integrate = "NR")
+glgm1 <- glgm(model, cov.model = "matern", kappa = log(0.5), inits = init, data=d, coords = d[,c("x","y")],nugget=TRUE, family="binomial", ntrial = 1,method.optim = "BFGS", method.integrate = "NR")
 
 ### prediction
 pred_glgm<-prediction(glgm1,as.data.frame(coordinates(g)))
+
+# I think the prediction function does not allow covariates
 
 
 
@@ -159,11 +216,11 @@ g$gsif<-pred_gsif@predicted$Attack
 gg<-as(g,"SpatialPolygonsDataFrame")
 
 par(mfrow=c(2,2),mar=c(0,0,0,0),oma=c(0,0,0,3))
-plot(gg,col=gray(g$glm),border=gray(g$glm));text(par("usr")[1],par("usr")[4]-5000,"glm",xpd=TRUE,adj=c(0,1))
-plot(gg,col=gray(g$mcml),border=gray(g$mcml));text(par("usr")[1],par("usr")[4]-5000,"mcml",xpd=TRUE,adj=c(0,1))
-plot(gg,col=gray(g$glgm),border=gray(g$glgm));text(par("usr")[1],par("usr")[4]-5000,"glgm",xpd=TRUE,adj=c(0,1))
-plot(gg,col=gray(g$gsif),border=gray(g$gsif));text(par("usr")[1],par("usr")[4]-5000,"gsif",xpd=TRUE,adj=c(0,1))
-legend("right",col=gray(0:10/10),legend=round(0:10/10,1),border=NA,pt.cex=2.8,pch=15,bty="n")
+plot(gg,col=gray(1-g$glm),border=gray(1-g$glm));text(par("usr")[1],par("usr")[4]-5000,"glm",xpd=TRUE,adj=c(-1,1),cex=3)
+plot(gg,col=gray(1-g$mcml),border=gray(1-g$mcml));text(par("usr")[1],par("usr")[4]-5000,"mcml",xpd=TRUE,adj=c(-1,1),cex=3)
+plot(gg,col=gray(1-g$glgm),border=gray(1-g$glgm));text(par("usr")[1],par("usr")[4]-5000,"glgm",xpd=TRUE,adj=c(-1,1),cex=3)
+plot(gg,col=gray(1-g$gsif),border=gray(1-g$gsif));text(par("usr")[1],par("usr")[4]-5000,"gsif",xpd=TRUE,adj=c(-1,1),cex=3)
+legend("right",col=gray(1-(0:10/10)),legend=round((0:10/10),1),border=NA,pt.cex=2.8,pch=15,bty="n")
 
 
 #######################################
@@ -206,61 +263,9 @@ lines(ecdf(pred.MCML$samples[ind.S[1], 5001:10000]),col = 2, lty = "dashed")
 
 
 
-#########################################
-### import rasters
-#########################################
-
-library(raster)
-library(sp)
-library(rgdal)
-library(rgeos)
-library(velox)
 
 
-r1<-"C:/Users/User/Documents/Lou/Mature_forest_2015_include_bajos_secondary.tif"
-r2<-"C:/Users/User/Documents/Lou/Mature_forest_2015_not_include_bajos_secondary.tif"
-r3<-"C:/Users/User/Documents/Lou/Landcover_2015_extended.tif"
 
-r <- stack(r3)
-#rb<-brick(r)
-#plot(r)
-
-ras <- raster(xmn=bbox(r)[1,1],xmx=bbox(r)[1,2],ymn=bbox(r)[2,1],ymx=bbox(r)[2,2],ncols=50,nrows=50)
-ras[] <- runif(ncell(ras))
-plot(ras)
-grid<-as(ras,"SpatialPixelsDataFrame")
-gridb<-gBuffer(SpatialPoints(coordinates(grid)),width=1000,byid=TRUE)
-plot(gridb,add=TRUE)
-
-x<-runif(100,bbox(r)[1,1],bbox(r)[1,2])
-y<-runif(100,bbox(r)[2,1],bbox(r)[2,2])
-p<-SpatialPoints(cbind(x,y))
-p<-gBuffer(p,width=1000,byid=TRUE)
-
-p<-gridb
-proj4string(p)<-proj4string(r)
-plot(p,add=TRUE)
-text(coordinates(p)[,1],coordinates(p)[,2],1:length(p))
-#e<-extract(ras,p)
-
-v<-velox(r)
-res<-v$extract(p,fun=function(x){paste(x,collapse="_")})
-
-ans<-lapply(res,function(i){
-  table(unlist(strsplit(i,"_")))
-})
-
-ans<-sapply(ans,function(i){
-	k<-names(i)==13
-	if(any(k)){
-	  i[k]/sum(i)
-	}else{
-	  0
-	}
-})
-
-g<-grid
-g$P_indFor_tg<-100*ans
 
 
 
