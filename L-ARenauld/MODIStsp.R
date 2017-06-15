c2682c6b191d37c522ac77f56032121e0c6b5286
frousseu <francois.rousseu@usherbrooke.ca>
2017-06-15 17:26:55 -0400

    started intergrating precise julian day and dates in plots

--- L-ARenauld/MODIStsp.R
+++ L-ARenauld/MODIStsp.R
@@ -24,7 +24,8 @@ for(i in seq_along(l)){
 }
 rm(raster_ts)
 
-r<-r5
+rv<-r5 #r5 is the initial raster used in plot ndvi3
+rd<-r3
 #v<-velox(r[[1:100]])
 
 ram<-readOGR("C:/Users/rouf1703/Documents/UdeS/Consultation/L-ARenaud/Doc",layer="ram")
@@ -32,94 +33,74 @@ plot(ram)
 ram<-gBuffer(ram,width=-0.01)
 plot(ram,add=TRUE)
 
-ee<-extract(r,ram)
+erv<-extract(rv,ram)
+erd<-extract(rd,ram)
 #ee<-do.call("cbind",lapply(strsplit(v$extract(ram,fun=function(i){paste(i,collapse="_")}),"_"),as.integer))
-ee<-ee[[1]]
+erv<-erv[[1]]
+erd<-erd[[1]]
+
 day<-substr(seq.Date(as.Date("2008-01-01"),as.Date("2008-12-31"),by=1),6,10)
-xd<-as.Date(sapply(strsplit(dimnames(ee)[[2]],"_"),function(i){
+julp<-sapply(strsplit(dimnames(erv)[[2]],"_"),function(i){
+  as.integer(i[4]) 
+})
+datep<-as.Date(sapply(strsplit(dimnames(erv)[[2]],"_"),function(i){
   paste(i[3],day[as.integer(i[4])],sep="-")  
 }))
-ind<-sapply(strsplit(dimnames(ee)[[2]],"_"),function(i){
+ind<-sapply(strsplit(dimnames(erv)[[2]],"_"),function(i){
   i[2]
 })
-sat<-substr(sapply(strsplit(dimnames(ee)[[2]],"_"),function(i){
+sat<-substr(sapply(strsplit(dimnames(erv)[[2]],"_"),function(i){
   i[1]
 }),1,3)
-ee<-t(apply(ee,1,function(i){identity(i)}))
-
-
-#######################################
-### double logistic
-
-d<-as.data.table(data.frame(x=jitter(rep(1:ncol(ee),nrow(ee)),factor=0),xd=xd,ind=ind,sat=sat,year=as.integer(substr(xd,1,4)),y=as.vector(t(ee))))
-d<-d[,.(xd,y,ind,sat,year,median=quantile(y,0.5,na.rm=TRUE),mean=mean(y,na.rm=TRUE)),by=.(x)]
-d<-d[d$year>=2003,]
-d$x<-as.numeric(factor(d$xd))
-d$date<-as.numeric(as.factor(substr(d$xd,6,10)))
-d$y2<-ifelse(d$y<=0,0,d$y)
-d<-as.data.frame(d)
-
-dd<-d[d$xd>="2004-01-01" & d$xd<"2004-12-31",]
-plot(dd$x,dd$y,col=gray(0,0.1))
-#nls(y2~(L/1+exp(-k*(date-x0))),data=as.data.frame(d)[samp,],start=list(L=6000,k=1,x0=6))
-m1<-nls(y2~SSlogis(x,Asym,xmid,scal),data=as.data.frame(dd))
-m2<-nlrq(y2~SSlogis(x,Asym,xmid,scal),data=as.data.frame(dd),tau=0.5)
-m3<-loess(y2~x,data=as.data.frame(dd),span=0.3)
-se<-unique(dd$x)
-lines(se,predict(m1,data.frame(x=se)))
-lines(se,predict(m2,data.frame(x=se)))
-lines(se,predict(m3,data.frame(x=se)))
-
-#ddd<-ddply(dd,.(
-#m<-modelNDVI(dd$y,year=2004,method="DLogistic")
-#for (i in m){ plot(i) } 
+#erv<-t(apply(erv,1,function(i){identity(i)}))
 
 
 ########################################
 ### raw images and models
 
-d<-as.data.table(data.frame(x=jitter(rep(1:ncol(ee),nrow(ee)),factor=0),xd=xd,ind=ind,sat=sat,year=as.integer(substr(xd,1,4)),y=as.vector(t(ee))))
+d<-as.data.table(data.frame(x=jitter(rep(1:ncol(erv),nrow(erv)),factor=0),datep=datep,julp=julp,ind=ind,sat=sat,year=as.integer(substr(datep,1,4)),y=as.vector(t(erv)),jul=as.vector(t(erd))),date)
 d$y<-d$y/10000
-d<-d[,.(xd,y,ind,sat,year,median=quantile(y,0.5,na.rm=TRUE),mean=mean(y,na.rm=TRUE)),by=.(x)]
+d<-d[,.(datep,y,ind,sat,year,julp,jul,median=quantile(y,0.5,na.rm=TRUE),mean=mean(y,na.rm=TRUE)),by=.(x,jul)]
+d$date<-as.Date(paste(d$year,day[d$jul],sep="-"))
 d<-d[d$year>=2003,]
-d$x<-as.numeric(factor(d$xd))
+d$x<-as.numeric(factor(d$datep))
+d$datex<-as.numeric(d$date)
 d<-as.data.frame(d)
 
 
-
-png("C:/Users/rouf1703/Documents/UdeS/Consultation/L-ARenaud/Doc/ndvi3.png",width=22,height=10,units="in",res=300)
+png("C:/Users/rouf1703/Documents/UdeS/Consultation/L-ARenaud/Doc/ndvi4.png",width=22,height=10,units="in",res=300)
 par(mar=c(7,4,4,4))
-plot(d$x,d$y,col=gray(0,0.1),xaxt="n",type="n",xlab="Date",ylab="NDVI")
+plot(d$date,d$y,col=gray(0,0.1),xaxt="n",xlab="Date",ylab="NDVI",type="p")
 
 years<-seq_along(unique(d$year))
-periods<-seq_along((unique(substr(d$xd,6,10))))
+periods<-seq_along((unique(substr(d$datep,6,10))))
 invisible(lapply(years,function(i){
    rect(xleft=periods-0.5+(i-1)*max(periods),ybottom=-2000,xright=periods+0.5+(i-1)*max(periods),ytop=12000,col=alpha("grey20",periods/max(periods)/4),border=NA,xpd=FALSE)
 }))
 
-axis(1,at=d$x[!duplicated(d$x)][seq(1,length(d$x),by=4)],labels=d$xd[!duplicated(d$x)][seq(1,length(d$x),by=4)],las=2,cex.axis=0.5)
-#axis.Date(1, at=unique(d$xd), format="%Y-%m-%d",las=2,cex.axis=0.5)
+#axis(1,at=d$x[!duplicated(d$x)][seq(1,length(d$x),by=4)],labels=d$datep[!duplicated(d$x)][seq(1,length(d$x),by=4)],las=2,cex.axis=0.5)
+axis.Date(1, at=seq(min(d$date,na.rm=TRUE),max(d$date,na.rm=TRUE),by="1 month"), format="%Y-%m-%d",las=2,cex.axis=0.5)
 
-points(d$x,d$y,col=gray(0,0.03))
-d2<-unique(d[,c("x","mean","sat")])
-points(d2$x,d2$mean,col=alpha("green4",0.85),pch=ifelse(d2$sat=="MOD",16,17),cex=1)
+points(d$date,d$y,col=gray(0,0.03))
+d2<-unique(d[,c("date","mean","sat")])
+points(d2$date,d2$mean,col=alpha("green4",0.85),pch=ifelse(d2$sat=="MOD",16,17),cex=1)
 #lines(d$x,d$median,col="darkgreen",lwd=2)
 
 sa<-d$sat%in%c("MYD","MOD")
-m1<-gam(y~s(x,k=100),data=d[sa,])
-m2<-loess(y~x,data=d[sa,],span=0.03)
-sx<-seq(0,max(d$x),by=1)
-p1<-predict(m1,data.frame(x=sx),type="response")
-p2<-predict(m2,data.frame(x=sx),type="response")
+m1<-gam(y~s(datex,k=100),data=d[sa,])
+m2<-loess(y~datex,data=d[sa,],span=0.03)
+sx<-seq(min(d$datex,na.rm=TRUE),max(d$datex,na.rm=TRUE),by=1)
+p1<-predict(m1,data.frame(datex=sx),type="response")
+p2<-predict(m2,data.frame(datex=sx),type="response")
 lines(sx,p1,col=alpha("blue",0.35),lwd=4)
 lines(sx,p2,col=alpha("red",0.35),lwd=4)
 
 invisible(lapply(years,function(i){
   year<-unique(d$year)[i]
   ### up
-  dd<-d[d$xd>=paste0(year-1,"-12-01") & d$xd<=paste0(year,"-09-30"),]
-  m1<-nls(y~Asym/(1+exp((xmid-x)/scal))+c,data=dd,start=list(Asym=0.5,xmid=quantile(dd$x,0.5),scal=1.2,c=0.2),control=list(minFactor=1e-12,maxiter=50),lower=c(0.3,0,0.8,0.1),upper=c(0.9,10000,3,0.3),algorithm="port")
-  se<-unique(dd$x)
+  dd<-d[d$datep>=paste0(year-1,"-12-01") & d$datep<=paste0(year,"-09-30"),]
+  m1<-nls(y~Asym/(1+exp((xmid-datex)/scal))+c,data=dd,start=list(Asym=0.5,xmid=quantile(dd$datex,0.5,na.rm=TRUE),scal=1.2,c=0.2),control=list(minFactor=1e-12,maxiter=50),lower=c(0.3,12000,0.8,0.1),upper=c(0.9,18000,3,0.3),algorithm="port")
+  se<-unique(dd$datex)
   se<-se[-c(1,2,(length(se)-1):length(se))]
   lines(se,predict(m1,data.frame(x=se)),col=alpha("green4",0.85),lwd=4)
   ### down
@@ -133,10 +114,10 @@ invisible(lapply(years,function(i){
     lines(rep(j,2),c(-0.3,-0.2),col="green4",lwd=1)
   }))
   
-  dd<-d[d$xd>=paste0(year,"-07-01") & d$xd<=paste0(year+1,"-03-01"),]
-  m2<-nls(y~Asym/(1+exp((xmid-x)/scal))+c,data=dd,start=list(Asym=-co$Asym,xmid=quantile(dd$x,0.75),scal=co$scal,c=co$Asym+co$c),control=list(minFactor=1e-12,maxiter=50),lower=c(-0.9,0,0.8,co$Asym+co$c),upper=c(-0.1,1000,3,co$Asym+co$c),algorithm="port")
+  dd<-d[d$datep>=paste0(year,"-07-01") & d$datep<=paste0(year+1,"-03-01"),]
+  m2<-nls(y~Asym/(1+exp((xmid-datex)/scal))+c,data=dd,start=list(Asym=-co$Asym,xmid=quantile(dd$datex,0.75,na.rm=TRUE),scal=co$scal,c=co$Asym+co$c),control=list(minFactor=1e-12,maxiter=50),lower=c(-0.9,12000,0.8,co$Asym+co$c),upper=c(-0.1,18000,3,co$Asym+co$c),algorithm="port")
   #m2<-nls(y~Asym/(1+exp((xmid-x)/scal))+c,data=dd,start=list(Asym=-coef(m1)["Asym"],xmid=quantile(dd$x,0.75),scal=1.3,c=0.16),control=list(minFactor=1e-12,maxiter=50),lower=c(-coef(m1)["Asym"],0,0.8,0.16),upper=c(-coef(m1)["Asym"],10000,3,0.16),algorithm="port")
-  se<-unique(dd$x)
+  se<-unique(dd$datex)
   se<-se[-c(1,2,3,4,(length(se)-3):length(se))]
   lines(se,predict(m2,data.frame(x=se)),col=alpha("green4",0.85),lwd=4)
   #lines(se,0.35/(1+exp((292-se)/0.8))+0.16,col="black",lwd=8)
