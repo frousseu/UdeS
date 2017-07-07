46fce24d30b107c989e2c142c05db9a80a8f2099
frousseu <francois.rousseu@usherbrooke.ca>
2017-07-07 14:23:28 -0400

    added fitGau and parameter outputs,more plotting options

--- L-ARenauld/NDVI.R
+++ L-ARenauld/NDVI.R
@@ -2,6 +2,7 @@
 library(foreach)
 library(doParallel)
 library(signal)
+library(robustbase)
 
 
 ### This script is for extracting ndvi/evi metrics from RasterStack objects using a set of regions defined by polygons
@@ -48,21 +49,21 @@ fitLog<-function(x,mmdate=c("12-01","09-15"),plot=FALSE){
   l<-lapply(years,function(i){
     paste(c(i-1,i),mmdate,sep="-")  
   })
-  peak<-sapply(l,function(i){
+  peak<-lapply(l,function(i){
     sx<-x[which(names(x)>=i[1] & names(x)<=i[2])]
     d<-data.frame(y=sx,x=as.integer(as.Date(names(sx))))
     min_xmid<-as.integer(as.Date(i[1])+120)
-    max_xmid<-as.integer(as.Date(i[2])-60)
-    lo1<-list(Asym=0,xmid=min_xmid,scal=5,c=0.0)
-    up1<-list(Asym=1,xmid=max_xmid,scal=30,c=0.8)
-    m1<-tryCatch(nls(y~Asym/(1+exp((xmid-x)/scal))+c,data=d,start=list(Asym=0.5,xmid=min_xmid+(max_xmid-min_xmid)/2,scal=10,c=0.2),control=list(minFactor=1e-12,maxiter=500),lower=lo1,upper=up1,algorithm="port"),error=function(j){TRUE})
+    max_xmid<-as.integer(as.Date(i[2])-75)
+    lo1<-list(Asym=0,xmid=min_xmid,scal=15,c=0.0)
+    up1<-list(Asym=1,xmid=max_xmid,scal=40,c=0.8)
+    m1<-tryCatch(nls(y~Asym/(1+exp((xmid-x)/scal))+c,data=d,start=list(Asym=0.5,xmid=min_xmid+(max_xmid-min_xmid)/2,scal=20,c=0.2),control=list(minFactor=1e-12,maxiter=500),lower=lo1,upper=up1,algorithm="port"),error=function(j){TRUE})
     if(!isTRUE(m1)){
       se<-seq(min(d$x),max(d$x),by=1) 
       if(plot){  
         #plot(as.Date(d$x),d$y)
-        lines(as.Date(se),predict(m1,data.frame(x=se)),col=alpha("green4",0.85),lwd=4)
+        lines(as.Date(se),predict(m1,data.frame(x=se)),col=alpha("green4",0.5),lwd=4)
       }
-      coef(m1)["xmid"]
+      coef(m1)
     }else{
       NA  
     }
@@ -70,27 +71,27 @@ fitLog<-function(x,mmdate=c("12-01","09-15"),plot=FALSE){
   peak
 }
 
-fitGau<-function(x,mmdate=c("12-01","09-15"),plot=FALSE){
+fitGau<-function(x,mmdate=c("12-01","10-15"),plot=FALSE){
   years<-as.integer(unique(substr(names(x),1,4)))
   years<-years[years<=2016]
   l<-lapply(years,function(i){
     paste(c(i-1,i),mmdate,sep="-")  
   })
-  peak<-sapply(l,function(i){
+  peak<-lapply(l,function(i){
     sx<-x[which(names(x)>=i[1] & names(x)<=i[2])]
     d<-data.frame(y=sx,x=as.integer(as.Date(names(sx))))
-    min_mu<-as.integer(as.Date(i[1])+150)
-    max_mu<-as.integer(as.Date(i[2])-30)
-    #lo1<-list(mu=0,xmid=min_xmid,scal=5,c=0.0)
-    #up1<-list(mu=1,xmid=max_xmid,scal=30,c=0.8)
-    m1<-tryCatch(nls(y~(1/(sigma*sqrt(2*pi)))*exp(-((x-mu)^2)/(2*sigma^2))+c,data=d,start=list(mu=min_mu,sigma=50,c=0.2),control=list(minFactor=1e-12,maxiter=500)),error=function(j){TRUE})
+    min_mu<-as.integer(as.Date(i[1])+120)
+    max_mu<-as.integer(as.Date(i[2])-0)
+    #lo1<-list(c=0.0)
+    #up1<-list(c=0.7)
+    m1<-tryCatch(nls(y~(a/(sigma*sqrt(2*pi)))*exp(-((x-mu)^2)/(2*sigma^2))+c,data=d,start=list(a=100,mu=max_mu-30,sigma=5,c=0.3),control=list(minFactor=1e-12,maxiter=500),algorithm="port"),error=function(j){TRUE})
     if(!isTRUE(m1)){
       se<-seq(min(d$x),max(d$x),by=1) 
       if(plot){  
         #plot(as.Date(d$x),d$y)
-        lines(as.Date(se),predict(m1,data.frame(x=se)),col=alpha("green4",0.85),lwd=4)
+        lines(as.Date(se),predict(m1,data.frame(x=se)),col=alpha("blue4",0.5),lwd=4)
       }
-      coef(m1)["xmid"]
+      coef(m1)
     }else{
       NA  
     }
@@ -107,7 +108,7 @@ fitGau<-function(x,mmdate=c("12-01","09-15"),plot=FALSE){
 
 ### Ram mountain
 ram<-readOGR("C:/Users/rouf1703/Documents/UdeS/Consultation/L-ARenaud/Doc",layer="ram")
-pol<-gBuffer(ram,width=0.5)
+pol<-gBuffer(ram,width=0.15)
 pol<-SpatialPolygonsDataFrame(pol,data.frame(id=1),match.ID=FALSE)
 
 
@@ -130,7 +131,8 @@ l<-strsplit(names(modis),"_")
 modis_doy<-as.Date(paste0(sapply(l,"[",3),"-01-01"))+as.integer(sapply(l,"[",4))-1
 
 m<-match("MYD",substr(names(modis),1,3))
-keep<-m:length(names(modis))
+#keep<-m:length(names(modis))
+keep<-1:length(names(modis))
 
 ### subset modis to only get data from when Aqua was also used
 modis<-subset(modis,keep)
@@ -180,9 +182,9 @@ gimms_jul<-stack(setValues(gimms,as.integer(format(rep(gimms_doy,each=ncell(gimm
 #############################################################
 ##### Extract raster values for each region #################
 
-r<-modis # determine series to use
-rd<-modis_jul
-doy<-modis_doy
+r<-gimms # determine series to use
+rd<-gimms_jul
+doy<-gimms_doy
 
 
 registerDoParallel(6) 
@@ -218,7 +220,7 @@ vd<-foreach(i=1:length(pol),.packages=c("raster","sp")) %dopar% {
 peak_cell<-lapply(seq_along(v),function(i){
   lapply(1:nrow(v[[i]]),function(j){
     pl<-FALSE
-    val<-v[[i]][j,]/10000
+    val<-v[[i]][j,]/1
     jul<-vd[[i]][j,]
     names(jul)<-doy
     #jul2<-as.integer(sapply(strsplit(names(jul),"_"),"[",4))
@@ -231,39 +233,49 @@ peak_cell<-lapply(seq_along(v),function(i){
     names(val)<-name
     o<-order(name)
     val<-val[o]
-    s1<-sgolayfilt(na.spline(val),n=81,p=3,m=1)
+    s1<-sgolayfilt(na.spline(val),n=41,p=3,m=1)
     names(s1)<-names(val)
+    #pos<-findminmax(s1,n=5,beg="03-01",end="07-01")
+    #sg<-as.Date(sapply(pos,function(k){mean(as.Date(names(s1)[k]))}))
     pos<-unlist(findminmax(s1,n=1,beg="03-01",end="07-01"))
     sg<-as.Date(names(s1)[pos])
     s0<-sgolayfilt(na.spline(val),n=21,p=3,m=0)
-    if(pl){  
-      plot(as.Date(names(val)),val,ylim=c(-0.2,1))
-      lines(as.Date(names(val)),s0)
-      lines(as.Date(names(val)),s1*3)
-      abline(0,0)
-    }
     #data.frame(na.spline(v[[i]][j,]),doy,names(s1),max=as.numeric(seq_along(doy)%in%pos)) # verif
     if(!j%%20)
       print(j)
-    logi<-as.Date(fitLog(val[!is.na(val)],plot=pl))[-1]
-    rbind(sg,logi)
+    if(pl){  
+      plot(as.Date(names(val)),val,ylim=c(-0.2,1))
+      lines(as.Date(names(val)),s0)
+      points(as.Date(names(val)),s1*7,col="red",cex=0.5)
+      abline(0,0)
+    }
+    mLog<-fitLog(val[!is.na(val)],plot=pl)[-1] #teke out firt year for gimms
+    logi<-as.Date(sapply(mLog,function(k){k["xmid"]}))
+    #xx<<-val[!is.na(val)]
+    #gaus<-as.Date(fitGau(val[!is.na(val)],plot=pl))[-1]
+    if(pl){  
+      h<-sapply(mLog,function(k){k["c"]})+sapply(mLog,function(k){k["Asym"]})/2
+      points(sg,h,col="red",pch=16)
+      points(logi,h,col="green4",pch=16)
+    }
+    rbind(sg,logi)#,gaus)
   })
 })
 
 peak1<-lapply(peak_cell,function(i){
   ii<-do.call("rbind",lapply(i,function(j){j[1,]}))
-  as.Date(colMeans(ii,na.rm=TRUE),origin="1970-01-01")  
+  as.Date(colMedians(ii,na.rm=TRUE),origin="1970-01-01")  
 })
 peak2<-lapply(peak_cell,function(i){
   ii<-do.call("rbind",lapply(i,function(j){j[2,]}))
-  as.Date(colMeans(ii,na.rm=TRUE),origin="1970-01-01")  
+  as.Date(colMedians(ii,na.rm=TRUE),origin="1970-01-01")  
 })
 
-#peak1g<-peak1
-#peak2g<-peak2
+peak1g<-peak1
+peak2g<-peak2
 
-peak1m<-peak1
-peak2m<-peak2
+#peak1m<-peak1
+#peak2m<-peak2
 
 ### gimms and modis compare (peak1 peak2)
 
@@ -272,8 +284,16 @@ j1g<-as.Date(as.integer(format(peak1g[[1]],"%j")))
 j2g<-as.Date(as.integer(format(peak2g[[1]],"%j")))
 j1m<-as.Date(as.integer(format(peak1m[[1]],"%j")))
 j2m<-as.Date(as.integer(format(peak2m[[1]],"%j")))
+ylim<-range(as.Date(c("1970-04-01","1970-07-01")))
 
-plot(as.integer(substr(peak1g[[1]],1,4)),j1g,pch=16,col=alpha("red",0.3),ylim=range(as.Date(c("1970-04-01","1970-07-01"))),type="l",las=2,lwd=4)
+
+#j1g<-scale(j1g)[,1]
+#j2g<-scale(j2g)[,1]
+#j1m<-scale(j1m)[,1]
+#j2m<-scale(j2m)[,1]
+#ylim<-range(c(j1g,j2g))
+
+plot(as.integer(substr(peak1g[[1]],1,4)),j1g,pch=16,col=alpha("red",0.3),ylim=ylim,type="l",las=2,lwd=4,xlim=c(1980,2015))
 lines(as.integer(substr(peak2g[[1]],1,4)),j2g,pch=16,col=alpha("red",0.3),lwd=4,lty=2)
 lines(as.integer(substr(peak1m[[1]],1,4)),j1m,pch=16,col=alpha("blue",0.3),ylim=range(c(j1m,j2m)),type="l",las=2,lwd=4)
 lines(as.integer(substr(peak2m[[1]],1,4)),j2m,pch=16,col=alpha("blue",0.3),lwd=4,lty=2)
