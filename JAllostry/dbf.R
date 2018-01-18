99178795ce79796eb81c049b44b215502577ecc0
frousseu <francois.rousseu@usherbrooke.ca>
2018-01-18 11:35:29 -0500

    4 vars together

--- JAllostry/dbf.R
+++ JAllostry/dbf.R
@@ -1,34 +1,43 @@
 library(foreign)
 library(data.table)
 
-path<-"C:/Users/rouf1703/Documents/UdeS/Consultation/JAllostry/Doc/Pieges_5km/Pieges_5km/TMOY" # new files
-x<-list.files(path,pattern=".dbf") # liste les fichiers dans le chemin
-l<-sapply(x,function(i){read.dbf(file.path(path,i))}) # lit tous les fichiers et les sauvegarde dans une liste
+path<-"C:/Users/rouf1703/Documents/UdeS/Consultation/JAllostry/Doc/Pieges_5km/Pieges_5km"
+dos<-c("PRTOT","TMOY","TMIN","TMAX") # list folders and variables
+d<-vector(mode="list",length=length(dos)) # create list to store data
+
+for(j in seq_along(dos)){
+
+  x<-list.files(file.path(path,dos[j]),pattern=".dbf",full.names=TRUE) # liste les fichiers dans le chemin
+  l<-sapply(x,function(i){read.dbf(i)}) # lit tous les fichiers et les sauvegarde dans une liste
+
+  g<-grep("030101",names(l[[1]])) # Ceci cherche le premier janvier et toutes les colonnes précédentes sont répétées
+  keep<-names(l[[1]])[1:(g-1)] # Ce sont les colonnes à conserver et répéter (le buffer de 5km est sûrement à enlever)
+
+  ld<-lapply(l,function(i){
+    res<-i[rep(1:nrow(i),ncol(i)-length(keep)),keep] # répète les infos communes
+    res$id<-gsub("P|TY|TN|TX","",rep(names(i)[g:ncol(i)],each=nrow(i))) # eliminate prefix from date data
+    res<-cbind(res,unlist(i[,g:ncol(i)],use.names=FALSE),stringsAsFactors=FALSE)
+    names(res)[ncol(res)]<-dos[j] # assign variable name
+    res
+  })
+  d[[j]]<-rbindlist(ld) # ramène tous dans un même data.frame à partir de la liste ld
+
+}
+
+d<-Reduce(function(...){merge(...,all=TRUE)},d) # merge four variables in a single data.frame
+temp<-strsplit(d$id, "(?<=.{2})", perl = TRUE) # split date info to construct an ok date
+d$date<-sapply(temp,function(i){paste(paste0(c("20","",""),i),collapse="-")}) # construct date as a character
 
 
-ld<-lapply(l,function(i){
-  res<-i[rep(1:nrow(i),ncol(i)-3),c(1:3)] # répète les infos communes
-  res$id<-rep(names(i)[-(1:3)],each=nrow(i))
-  res$temp<-unlist(i[,4:ncol(i)],use.names=FALSE)
-  res
-})
-d<-rbindlist(ld) # ramène tous dans un même data.frame à partir de la liste ld
+### graph to check the time series
+s<-split(d,d$Site_Seq)[1:10] # choose the first 10 sites for faster plotting
+plot(as.Date(s[[1]]$date),rep(0,nrow(s[[1]])),ylim=c(-35,35),type="n")
+invisible(lapply(s,function(i){
+   lines(as.Date(i$date),i$TMOY) 
+}))
 
 
-### graph
-s<-split(d,d$Site_Seq)
-plot(1:365,ylim=c(-30,25),type="n")
-lapply(s,function(i){
-   lines(i$temp,col=gray(0,0.1)) 
-})
 
 
-### splitting info
-s<-d$id
-x<-strsplit(s, "(?<=.{2})", perl = TRUE)
 
-d$type<-sapply(x,"[",1)
-d$year<-sapply(x,"[",2)
-d$month<-sapply(x,"[",3)
-d$day<-sapply(x,"[",4)
 
