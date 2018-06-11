f884b52837026a74b64b2bbf5d8672d127e6539c
frousseu <francois.rousseu@usherbrooke.ca>
2018-06-11 16:53:15 -0400

    toseq, newdata

--- GStecher/fire_occ.R
+++ GStecher/fire_occ.R
@@ -12,6 +12,55 @@ library(RColorBrewer)
 #library(brinla)
 library(visreg)
 
+toseq<-function(x,n=100){
+  if(is.numeric(x)){
+    r<-range(x,na.rm=TRUE)
+    seq(r[1],r[2],length.out=n)
+  }else{
+    sort(unique(x))  
+  }
+}
+
+newdata<-function(x,v=names(x),n=100,fun=mean,list=FALSE){
+  
+  # returns the mean or the levels
+  mm<-function(y){
+    if(is.numeric(y)){
+      fun(y)
+    }else{
+      names(rev(sort(table(y))))[1]
+    }
+  }
+  ## possibly a bug with the function did not save the last part
+  ans<-lapply(v,function(i){
+    if(n==1L){
+      val<-mm(x[,i])
+    }else{
+      val<-toseq(x[,i],n=n)
+    }
+    l<-lapply(x[,setdiff(names(x),i),drop=FALSE],mm)
+    res<-data.frame(val,as.data.frame(l),stringsAsFactors=FALSE)
+    names(res)[1]<-i
+    if(list){ # inefficient, should not be turned to data.frame if list=TRUE
+      as.list(res)
+    }else{
+      res
+    } 
+  })  
+  names(ans)<-v
+  
+  if(length(v)==1L){
+    unlist(ans,recursive=FALSE)
+  }else{
+    ans
+  }
+  
+}
+
+#l<-newdata(d)
+
+
+
 load("~/UdeS/Consultation/GStetcher/Doc/LLF_occur.RData")
 
 occ<-na.omit(llf.occur)
