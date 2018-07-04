5027532bb59db179bc3d9627abb7a17edb4a5717
frousseu <francois.rousseu@usherbrooke.ca>
2018-07-04 17:20:46 -0400

    comments

--- GStecher/fire_occ.R
+++ GStecher/fire_occ.R
@@ -9,10 +9,11 @@ library(INLA)
 library(rgeos)
 library(FRutils)
 library(RColorBrewer)
-#library(brinla)
 library(visreg)
 library(sf)
 
+######################################################################
+### function to create a sequence from the range of values in a vector
 toseq<-function(x,n=100){
   if(is.numeric(x)){
     r<-range(x,na.rm=TRUE)
@@ -22,6 +23,8 @@ toseq<-function(x,n=100){
   }
 }
 
+#####################################################################################
+### function to create a data.frame for predictions for each variable in a data.frame
 newdata<-function(x,v=names(x),n=100,fun=mean,list=FALSE){
   
   # returns the mean or the levels
@@ -58,67 +61,81 @@ newdata<-function(x,v=names(x),n=100,fun=mean,list=FALSE){
   
 }
 
-#l<-newdata(d)
-
-
-
+#############
+### load data
 load("~/UdeS/Consultation/GStetcher/Doc/LLF_occur.RData")
 
+############################################################################################
+### remove everything with NAs (temporary) and take a random sample to reduce computing time
 occ<-llf.occur
 occ<-na.omit(occ)
-occ<-occ[sample(1:nrow(occ),10000),] # sample location to reduce computing time
+occ<-occ[sample(1:nrow(occ),5000),] # sample location to reduce computing time
 
+######################################################
+### check the proportions of 0 and 1 in each VEGZONSNA
 tab<-table(occ$PA,occ$VEGZONSNA)
 tab[2,]/(tab[1,]+tab[2,])
 
+#####################################################################
+### convert high_name to factor and log transform the population size
 occ$high_name<-as.factor(occ$high_name)
 occ$logPop_2017<-log(occ$Pop_2017+0.2)
 
+#############################################
+### build a spatial object with the locations
 occs<-occ
 coordinates(occs)<-~Longitude+Latitude
 proj4string(occs)<-"+init=epsg:4326"
-
 prj<-"+proj=utm +zone=33 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
 occs<-spTransform(occs,CRS(prj))
 
-#plot(occs,col=alpha(ifelse(occs$PA==1,"red","blue"),0.25),pch=16)
-
-m1 <- glm (PA ~ -1 + VEGZONSNA * trees_age + WtrUrb_km + logPop_2017 + Road_dens + high_name, family = binomial(link = "logit"), data = na.omit(occ))
-#m1 <- glm (PA ~ -1+VEGZONSNA+logPop_2017+trees_age+Road_dens+WtrUrb_km, family = binomial(link = "logit"), data = na.omit(occ))
+#########################################
+### build a simple glm to compare results
+m1 <- glm (PA ~ VEGZONSNA + trees_age + WtrUrb_km + logPop_2017 + Road_dens + high_name, family = binomial(link = "logit"), data = na.omit(occ))
 par(mfrow=c(3,3),mar=c(4,4,3,3))
 visreg(m1,scale="response")
 par(mfrow=c(1,1))
-visreg(m1,"trees_age","VEGZONSNA",scale="response",overlay=FALSE)
 
+####################################################################
+### look at the variogram with the residuals from the previous model
 coords <- coordinates(occs)
 v<-variog(coords=coords,data=resid(m1),breaks=seq(0,100000,by=500),max.dist=100000,bin.cloud=TRUE)
-#fitv<-variofit(v,ini.cov.pars=c(0.2,30000),cov.model="exponential",fix.nugget=FALSE,nugget=0.125,fix.kappa=FALSE,kappa=0.45)
 plot(v, main = "Variogram for spatial autocorrelation (LFY, fire occurrence)",type="b") 
-#lines(fitv)
 
+##############
+### get sweden
 swe <- raster::getData("GADM", country = "SWE", level = 0)
 swe<-spTransform(swe,proj4string(occs))
 
-#prdomain <- inla.nonconvex.hull(coordinates(occs),convex=-0.02, resolution = c(100, 100))
+#############################################
+### build a mesh and use sweden as a boundary
+# smaller values put in here will make a more precise grid, but will take longer to run
 mesh<-inla.mesh.2d(loc=coordinates(occs),max.edge=c(20000,200000),offset=c(20000,50000),cutoff=20000,boundary=swe)
 plot(mesh,asp=1)
 
-#spde<-inla.spde2.matern(mesh,alpha=2)
+#############################
+### build spde with pc priors
 spde<-inla.spde2.pcmatern(mesh,prior.range=c(100000,0.9),prior.sigma=c(3,0.1))
 
-#m<-inla(model,data=list(y=occ$PA,intercept=rep(1,spde$n.spde),spatial=1:spde$n.spde),control.predictor=list(A=A,compute=TRUE),family="binomial")
-
-g<-makegrid(swe,n=20000) # makes sure pixels touching are included too
+###########################################################
+### build the raster/grid that will be used for predictions
+g<-makegrid(swe,n=20000)
 g<-SpatialPoints(g,proj4string=CRS(proj4string(occs)))
-#o<-over(as(g,"SpatialPolygons"),swe) # does not change much when the grid gets small
+#o<-over(as(g,"SpatialPolygons"),swe) # makes sure pixels touching are included too, does not change much when the grid gets small
 o<-over(g,swe)
 g<-SpatialPixels(g)
 g<-g[apply(o,1,function(i){!all(is.na(i))}),]
-#test<-st_intersects(st_as_sf(g),st_as_sf(swe))
 
+############################################
+### make spatial index to link with the spde
 s.index<-inla.spde.make.index(name="spatial",n.spde=spde$n.spde)
 
-#model<-PA~-1+VEGZONSNA+intercept+logPop_2017+trees_age+Road_dens+WtrUrb_km+f(spatial,model=spde)
+###########################
+### make observation matrix
+A<-inla.spde.make.A(mesh=mesh,loc=coordinates(occs))
+
+#############
+### model set
 
 modell<-list(
   PA ~ -1 + intercept + Road_dens + logPop_2017 + f(spatial,model=spde),
@@ -148,28 +165,31 @@ modell<-list(
   PA ~ -1 + intercept + Road_dens + logPop_2017 + WtrUrb_km + Frbreak_km * trees_age + high_name + VEGZONSNA + f(spatial,model=spde)
 )
 
-A<-inla.spde.make.A(mesh=mesh,loc=coordinates(occs))
-
-###################################
+###################
 ### model selection
+
+# this runs every model in the model set
 
 ml<-vector(mode="list",length=length(modell))
 
 for(i in seq_along(modell)){
-  
+  # list of variables
   v<-setdiff(all.vars(modell[[i]]),c("PA","intercept","spatial","spde"))
-  
+  # build the data stack
   stack.est<-inla.stack(data=list(PA=occ$PA),A=list(A,1),effects=list(c(s.index,list(intercept=1)),as.list(occ[,v,drop=FALSE])),tag="est")
-
+  # run the model with the eb strategy for faster runs (more approximate)
   ml[[i]]<-inla(modell[[i]],data=inla.stack.data(stack.est),control.predictor=list(A=inla.stack.A(stack.est)),family="binomial",control.compute=list(dic=TRUE,waic=TRUE,cpo=FALSE,config=FALSE,return.marginals=FALSE),control.inla=list(strategy='gaussian',int.strategy="eb"))
-  
+  # print iterations
   print(paste(" ",i,"/",length(ml)," "))
-
 }
 
+#######################################
+### compute dic and waic values
 dic<-sapply(ml,function(i){i$dic$dic})
 waic<-sapply(ml,function(i){i$waic$waic})
 
+#################################################
+### order models according to one of the criteria
 sel<-data.frame(waic,model=sapply(modell,function(i){format(deparse(i,width.cutoff=400))}))
 sel<-sel[order(sel[,1]),]
 head(sel)
@@ -177,36 +197,45 @@ head(sel)
 ####################################
 ### predictions
 
+#######################
+### find the best model
 b<-which.min(waic)
 modell[[b]]
 bmodel<-modell[[b]]
 
-
+###################################################################
+### build prediction matrices for the map and the prediction graphs
 Ap<-inla.spde.make.A(mesh=mesh,loc=coordinates(g))
-n<-100
-Apn<-inla.spde.make.A(mesh=mesh,loc=matrix(c(312180,6342453),ncol=2)[rep(1,n),,drop=FALSE])
+n<-100 # number of divisions in generated values for the focus variable
+Apn<-inla.spde.make.A(mesh=mesh,loc=matrix(c(312180,6342453),ncol=2)[rep(1,n),,drop=FALSE]) # the graphs are build using a random points in the area
 
+################################################
+### build newdata with variable values to submit
 v<-setdiff(all.vars(bmodel),c("PA","intercept","spatial","spde"))
 lp<-newdata(x=occ[,v,drop=FALSE],v=v,n=n,fun=median,list=TRUE)
 lpmed<-lapply(newdata(x=occ[,v,drop=FALSE],v=v,n=1,fun=median,list=TRUE)[[1]],function(i){rep(i,length(g))})
 
+########################################################
+### bind the data stack for the estimate and for the map
 stack.est<-inla.stack(data=list(PA=occ$PA),A=list(A,1),effects=list(c(s.index,list(intercept=1)),as.list(occ[,v,drop=FALSE])),tag="est")
-
 stack.map<-inla.stack(data=list(PA=NA),A=list(Ap,1),effects=list(c(s.index,list(intercept=1)),lpmed),tag="map")
-
 full.stack<-inla.stack(stack.est,stack.map)
 
+#######################################
+### add a stack for each focus variable
 for(i in seq_along(v)){
   le<-length(lp[[v[i]]][[1]])
   if(le!=n){
-    AA<-inla.spde.make.A(mesh=mesh,loc=matrix(c(819006,6545844),ncol=2)[rep(1,le),,drop=FALSE])
+    AA<-inla.spde.make.A(mesh=mesh,loc=matrix(c(819006,6545844),ncol=2)[rep(1,le),,drop=FALSE]) # for categorical variables
   }else{
-    AA<-Apn
+    AA<-Apn # for numerical variables
   }
   stack<-inla.stack(data=list(PA=NA),A=list(AA,1),effects=list(c(s.index,list(intercept=1)),lp[[v[i]]]),tag=v[i])     
   full.stack<-inla.stack(full.stack,stack)
 }
 
+##########################################
+### extract index for each stack
 index.est<-inla.stack.index(full.stack,tag="est")$data
 index.map<-inla.stack.index(full.stack,tag="map")$data
 index<-list(est=index.est,map=index.map)
@@ -215,10 +244,14 @@ for(i in seq_along(v)){
 }  
 names(index)[3:length(index)]<-v
 
+##################################################
+### rerun best model with each variable to predict
 m<-inla(bmodel,data=inla.stack.data(full.stack),control.predictor=list(A=inla.stack.A(full.stack),compute=TRUE,link=1),family="binomial",control.compute=list(dic=TRUE,waic=TRUE,cpo=TRUE,config=FALSE),control.inla=list(strategy='gaussian',int.strategy="eb"))
 
 
-###########################################
+##################################
+### build a relative frequency map
+
 ### raw predictions on probability of usage
 par(mfrow=c(1,4),oma=c(0,5,0,5))
 plot(swe,border=gray(0,0.25),lwd=0.01)
@@ -227,29 +260,24 @@ p<-m$summary.fitted.values[index[["map"]],"mean"]
 gp<-SpatialPixelsDataFrame(g,data=data.frame(p=p))
 rgp<-raster(gp)
 brks <- seq(min(p),max(p),by=0.01)
-#cols<-colo.scale(length(brks)-1,rev(brewer.pal(11,"RdYlGn")))
 cols<-colo.scale(300,rev(brewer.pal(11,"RdYlGn")))
-#plot(gp,breaks=brks,col=cols,at=pretty(brks,10))
 plot(swe)
 plot(rgp,col=cols,axes=FALSE,box="n",legend.shrink=1, legend.width=4,add=TRUE,axis.args=list(at=pretty(brks,n=10), labels=pretty(brks,n=10)),
-     legend.args=list(text='Probability of use', side=4, font=2, line=2.3))
+     legend.args=list(text='Probability of being an actual fire', side=4, font=2, line=2.3))
 plot(swe,add=TRUE,border=gray(0,0.25),lwd=0.01)
-###############################
+
 ### prediction on relative risk
 p<-m$summary.fitted.values[index[["map"]],"mean"]
 p<-p/(sum(occ$PA)/nrow(occ))
 gp<-SpatialPixelsDataFrame(g,data=data.frame(p=p))
 rgp<-raster(gp)
 brks <- seq(min(p),max(p),by=0.01)
-#cols<-colo.scale(length(brks)-1,rev(brewer.pal(11,"RdYlGn")))
 cols<-colo.scale(brks-1,rev(brewer.pal(11,"RdBu")))
-#plot(gp,breaks=brks,col=cols,at=pretty(brks,10))
 plot(swe)
 plot(rgp,col=cols,axes=FALSE,box="n",legend.shrink=1, legend.width=4,add=TRUE,axis.args=list(at=pretty(brks,n=10), labels=pretty(brks,n=10)),
      legend.args=list(text='Relative risk', side=4, font=2, line=2.3))
 plot(swe,add=TRUE,border=gray(0,0.25),lwd=0.01)
-#par(mfrow=c(1,1))
-##################################
+
 ### sd
 p<-m$summary.fitted.values[index[["map"]],"sd"]
 gp<-SpatialPixelsDataFrame(g,data=data.frame(p=p))
@@ -258,12 +286,13 @@ brks <- seq(min(p),max(p),by=0.01)
 cols<-colo.scale(300,rev(brewer.pal(11,"RdYlGn")))
 plot(swe)
 plot(rgp,col=cols,axes=FALSE,box="n",legend.shrink=1, legend.width=4,add=TRUE,axis.args=list(at=pretty(brks,n=10), labels=pretty(brks,n=10)),
-     legend.args=list(text='Sd of probability of use', side=4, font=2, line=2.3))
+     legend.args=list(text='sd of the probability of being an actual fire', side=4, font=2, line=2.3))
 plot(swe,add=TRUE,border=gray(0,0.25),lwd=0.01)
 
 
 #####################################
-### graphs predictions
+### graphical predictions
+
 par(mfrow=c(round(sqrt(length(v)),0),ceiling(sqrt(length(v)))),mar=c(4,4,3,3),oma=c(0,10,0,0))
 for(i in seq_along(v)){
   p<-m$summary.fitted.values[index[[v[i]]],c("0.025quant","0.5quant","0.975quant")]
@@ -283,14 +312,11 @@ mtext("Probability of being an actual fire",outer=TRUE,cex=1.2,side=2,xpd=TRUE,l
 
 
 #image(inla.mesh.project(mesh,field=m$summary.fitted.values[inla.stack.index(full.stack,tag="latent")$data,"mean"]),dims=c(10,10))
-
 projgrid <- inla.mesh.projector(mesh, dims=c(500,500))
 xmean <- inla.mesh.project(projgrid, m$summary.random$spatial$mean)
 xsd <- inla.mesh.project(projgrid, m$summary.random$spatial$sd)
 image(xmean,asp=2,col=heat.colors(100))
-
 res<-inla.spde2.result(m,"spatial",spde)
-
 plot(res[["marginals.range.nominal"]][[1]], type = "l",main = "Nominal range, posterior density")
 
 
