cd5cf924d18f4bb29c61b64d45bd9fdbd983a0c7
frousseu <francois.rousseu@usherbrooke.ca>
2019-05-06 11:55:53 -0400

    graphs overlapping

--- CPotvin/multifunc.R
+++ CPotvin/multifunc.R
@@ -186,7 +186,7 @@ ld[[1]]$year<-factor(ld[[1]]$year)
 coly<-c("brown3","cornflowerblue","chartreuse4")
 mgp<-c(3,0.25,0)
 tcl<--0.2
-ths<-seq(0.02,0.98,by=0.01)
+ths<-seq(0.05,0.95,by=0.01)
 
 par(mfrow=c(2,2),mar=c(4,4,1,3))
 
@@ -199,18 +199,18 @@ par(mfrow=c(2,2),mar=c(4,4,1,3))
   title(xlab="Diversity",line=1.2,cex.lab=1.2)
   axis(1,at=sort(unique(i$diversity)),label=sort(unique(i$diversity)),tcl=tcl,mgp=mgp)
   axis(2,at=0:(sum(names(i)%in%vars)),label=0:(sum(names(i)%in%vars)),las=2,tcl=tcl,mgp=mgp)
-  mtext(i$year[1],side=3,line=-1.5,font=2,adj=c(0.95))
+  #mtext(i$year[1],side=3,line=-1.5,font=2,adj=c(0.95))
   
   thr<-sort(unique(th$thresholds))
   l<-split(th,th$thresholds)
-  ramp<-c("grey85",coly,"grey1")
-  cols<-alpha(colo.scale(1:length(l),ramp),0.75)
-  co<-lapply(seq_along(l),function(j){
+  
+  #co<-lapply(seq_along(l),function(j){
+  for(j in seq_along(l)){  
     k<-l[[j]]
-    points(jitter(k$diversity,fac=0.5),jitter(k$funcMaxed,fac=1.25),col=cols[j],pch=1,cex=0.75,lwd=0.2) 
+    #points(jitter(k$diversity,fac=0.5),jitter(k$funcMaxed,fac=1.25),col=cols[j],pch=1,cex=0.75,lwd=0.2) 
     k$year<-as.factor(k$year)
     print(j)
-    m<-glm(funcMaxed~diversity*year,data=k,family=poisson(link="log"))
+    m<-glm(funcMaxed~diversity*year,data=k,family=poisson)
     print(summary(m))
     div<-seq(1,5,by=0.1)
     yb<-levels(k$year)
@@ -219,17 +219,20 @@ par(mfrow=c(2,2),mar=c(4,4,1,3))
     e$pred<-pred
     lp<-split(e,e$year)
     lapply(seq_along(lp),function(y){
+      ramp<-c("grey80",coly[y])
+      cols<-alpha(colo.scale(1:length(l),ramp),0.75)
       p<-lp[[y]]
-      lines(div,p$pred,col=cols[j],lwd=2,lty=y)
+      lines(div,p$pred,col=cols[j],lwd=2)#col=cols[j],lwd=2)
     })
     res<-as.data.frame(summary(m)$coef)
-    res<-cbind(th=k$thresholds[1],year=i$year[1],var=row.names(res),res,as.data.frame(confint(m)))
+    #res<-cbind(th=k$thresholds[1],year=i$year[1],var=row.names(res),res,as.data.frame(confint(m)))
     res
-  })
+    visreg2d(m,"diversity","year",scale="response")
+  }
   ## legend
   legend_image <- as.raster(matrix(alpha(colo.scale(1:length(l),ramp),0.75), ncol=1))
   rec<-c(par("usr")[2]+0.04,par("usr")[3],par("usr")[2]+0.13,par("usr")[4])
-  rasterImage(legend_image,rec[1],rec[2],rec[3],rec[4],xpd=TRUE,lwd=1)
+  #rasterImage(legend_image,rec[1],rec[2],rec[3],rec[4],xpd=TRUE,lwd=1)
   #at<-seq(min(ths),max(ths),length.out=10)
   at<-c(min(ths),seq(0.1,0.9,by=.1),max(ths))
   labs<-rescale(c(at,range(ths)),to=c(1,0))
